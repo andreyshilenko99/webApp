e36ff6ef6d94f0a03e297dd30de8a36e7c5de9ea
andreishilenko <asj931@yahoo.com>
2022-12-09 19:36:41 +0300

    tabs added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
+import Tab from "./components/Tab/Tab"
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
@@ -12,12 +13,12 @@ function App() {
     useEffect(() => {
         tg.ready();
     }, [])
-
     return (
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<ProductList />}/>
+                {/*<Route index element={<ProductList />}/>*/}
+                <Route index element={<Tab />}/>
                 <Route path={'form'} element={<Form />}/>
             </Routes>
         </div>
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,16 +4,8 @@ import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
-const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-]
+
+
 
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
@@ -21,7 +13,7 @@ const getTotalPrice = (items = []) => {
     }, 0)
 }
 
-const ProductList = () => {
+const ProductList = ({products}) => {
     const [addedItems, setAddedItems] = useState([]);
     const {tg, queryId} = useTelegram();
 
@@ -31,7 +23,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
